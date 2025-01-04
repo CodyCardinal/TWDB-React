9728e0bb58006e35f41eb7604657e909423fabe0
Cody Cardinal <cardinalc@gmail.com>
2025-01-03 17:16:57 -0800

    Functional State Update vs Direct State Update in Scorekeeper

--- AdvancedReact/src/App.jsx
+++ AdvancedReact/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Counter from './Counter.jsx';
+import ScoreKeeper from './ScoreKeeper.jsx';
 
 function App() {
   return (
     <>
       <h1>State Demo</h1>
       <Counter />
+      <ScoreKeeper />
     </>
   )
 }
---
+++ AdvancedReact/src/ScoreKeeper.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function ScoreKeeper() {
+    const [scores, setScores] = useState({ p1Score: 0, p2Score: 0 });
+    
+    function increaseP1ScoreDirectStateUpdate() {
+        const newScores = { ...scores, p1Score: scores.p1Score + 1 };
+        setScores(newScores);
+    }
+
+    function increaseP1ScoreFunctionalStateUpdate() {
+        setScores((oldScores) => {
+            return { ...oldScores, p1Score: oldScores.p1Score + 1 };
+        });
+    }
+
+    return (
+        <div>
+            <p>Player 1: {scores.p1Score}</p>
+            <p>Player 2: {scores.p2Score}</p>
+            <button onClick={increaseP1ScoreFunctionalStateUpdate}>+1 Player 1 FUNCTIONAL STATE UPDATE</button>
+            <button onClick={increaseP1ScoreDirectStateUpdate}>+1 Player 1 DIRECT STATE UPDATE</button>
+            <button>+1 Player 2</button>
+        </div>
+    );
+}
